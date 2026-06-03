0a0071082ec01daeefc0ac571e44a998c65c4d05
costahg <henriquecostag73@gmail.com>
2026-06-03 12:44:33 -0300

    sync

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "https://SEU-PROJETO.pages.dev",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/health")
+def health():
+    return {
+        "ok": True,
+        "message": "FastAPI respondeu com sucesso."
+    }
