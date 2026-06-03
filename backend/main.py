a7545674c2c9a5ce66b39f2ca5eec054bd2021d9
costahg <72760992+costahg@users.noreply.github.com>
2026-06-03 14:32:07 -0300

    Fix CORS origin URL format

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 origins = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
-    "https://main-3tj.pages.dev/",
+    "https://main-3tj.pages.dev",
 ]
 
 app.add_middleware(
